2ed5ae23015c9a279e7ed93436af9466e96308f6
Christian Dickreuter <christian.dickreuter@sv-technologie.de>
2023-05-09 08:35:42 +0200

    Implemented add Todo and Style

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,28 @@
 <template>
-  Todo App
+  <div class="chris-todo">
+    <div class="title has-text-centered">
+      Chris Todo
+    </div>
+
+    <div class="field has-addons">
+      <div class="control">
+        <input class="input" type="text" placeholder="Add a Todo">
+      </div>
+      <div class="control">
+        <a class="button is-info">
+          Add
+        </a>
+      </div>
+    </div>
+  </div>
 </template>
+
+<style>
+@import 'bulma/css/bulma.min.css';
+
+.chris-todo {
+  max-width: 400px;
+  padding: 20px;
+  margin: 0 auto;
+}
+</style>
