54f22890c309934f53f5f81edb82459bf9fde64b
Christian Dickreuter <christian.dickreuter@sv-technologie.de>
2023-05-17 15:42:24 +0200

    Implemented Todo list and Style

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
       Chris Todo
     </div>
 
-    <div class="field has-addons">
+    <div class="field has-addons mb-5">
       <div class="control">
         <input class="input" type="text" placeholder="Add a Todo">
       </div>
@@ -14,8 +14,34 @@
         </a>
       </div>
     </div>
+
+    <div
+      v-for="i in 3" 
+      class="card mb-5"
+    >
+      <div class="card-content">
+        <div class="content">
+          <div class="columns is-mobile is-vcentered">
+            <div class="column">
+              Einkaufen
+            </div>
+            <div class="column is-5 has-text-right">
+              <button class="button is-light">
+                &check;
+              </button>
+              <button class="button is-danger ml-2">
+                &cross;
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
+
+//Todo: Watch and code -> 5:28 Elements & Styles - Todo List
 
 <style>
 @import 'bulma/css/bulma.min.css';
