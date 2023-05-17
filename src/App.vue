51e6e51280cff0cdbc8cfaae9ebc92b8dedf0f3b
Christian Dickreuter <christian.dickreuter@sv-technologie.de>
2023-05-17 16:17:29 +0200

    Implemented add todo

--- src/App.vue
+++ src/App.vue
@@ -4,23 +4,33 @@
       Chris Todo
     </div>
 
-    <div class="field has-addons mb-5">
-      <div class="control">
-        <input class="input" type="text" placeholder="Add a Todo">
+    <form @submit.prevent="addTodo">
+      <div class="field has-addons mb-5">
+        <div class="control">
+          <input
+            v-model="newTodoContent"
+            class="input"
+            type="text"
+            placeholder="Add a Todo">
+        </div>
+        <div class="control">
+          <button
+            :disabled="!newTodoContent"
+            class="button is-info">
+            Add
+          </button>
+        </div>
       </div>
-      <div class="control">
-        <a class="button is-info">
-          Add
-        </a>
-      </div>
-    </div>
+    </form>
 
-    <div v-for="todo in todos" class="card mb-5">
+    <div
+      v-for="todo in todos"
+      class="card mb-5">
       <div class="card-content">
         <div class="content">
           <div class="columns is-mobile is-vcentered">
             <div class="column">
-              {{ todo.content}}
+              {{ todo.content }}
             </div>
             <div class="column is-5 has-text-right">
               <button class="button is-light">
@@ -43,28 +53,44 @@
   imports
 */
 import { ref } from "vue";
+import { v4 as uuidv4 } from "uuid";
 
 /* 
   todos
 */
-
 const todos = ref([
-  {
-    id: 'id1',
-    content: 'Einkaufen',
-    done: false
-  },
-  {
-    id: 'id2',
-    content: 'Staubsaugen',
-    done: true
-  },
-  {
-    id: 'id3',
-    content: 'Essen kochen',
+  // {
+  //   id: 'id1',
+  //   content: 'Einkaufen',
+  //   done: false
+  // },
+  // {
+  //   id: 'id2',
+  //   content: 'Staubsaugen',
+  //   done: true
+  // },
+  // {
+  //   id: 'id3',
+  //   content: 'Essen kochen',
+  //   done: false
+  // }
+])
+
+/* 
+  add todo
+*/
+const newTodoContent = ref('')
+
+const addTodo = () => {
+  const newTodo = {
+    id: uuidv4(),
+    content: newTodoContent.value,
     done: false
   }
-])
+
+  todos.value.unshift(newTodo)
+  newTodoContent.value = ''
+}
 
 </script>
 
